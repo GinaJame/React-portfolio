21a2ddb4e9c64025bfbdd6f034167b9f0759ce60
Gina <gina10111@hotmail.com>
2021-07-06 20:55:48 -0500

    First commit

---
+++ src/pages/mainPage/mainpage.component.jsx
@@ -0,0 +1,16 @@
+  
+import React from 'react';
+
+
+import './mainpage.styles.scss';
+
+const MainPage = () => (
+  <div className='homepage'>
+    <h1>Welcome to Gina´s portfolio</h1>
+    <p>Hi! My name is Gina, I´m a Computer Science student looking to 
+      learn new things 
+    </p>
+  </div>
+);
+
+export default MainPage;
---
+++ src/pages/projectsPage/projectspage.components.jsx
@@ -0,0 +1,12 @@
+  
+import React from 'react';
+
+import './projectspage.styles.scss';
+
+const ProjectsPage = () => (
+  <div className='homepage'>
+    <p>This is my home page</p>
+  </div>
+);
+
+export default ProjectsPage;
